063128a2dbca5e2ee8c1e88d0878a73d0d36accc
Shaahin Shahbazi <shaahin@gmail.com>
2015-08-13 13:29:16 -0500

    Retrieve data for listPage

--- routes.js
+++ routes.js
@@ -7,3 +7,14 @@ Router.route('/', {
     name: 'home',
     template: 'home'
 });
+
+// the colon is telling Iron Router that this part of the URL is a dynamic parameter.
+Router.route('/list/:_id', {
+  template: 'listPage',
+  //By using the “data” option, we’re able to pass data into a route from within the associated function
+  data: function() {
+    //console.log(this.params.someParameter);
+    var currentList = this.params._id;
+    return Lists.findOne({ _id: currentList });
+  }
+});
