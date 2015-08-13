4517c27321eae7fbdd6038df57969258b1a56252
Shaahin Shahbazi <shaahin@gmail.com>
2015-08-13 15:18:01 -0500

    Using pathFor for listPage without explicitly mentioning id

--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ Router.route('/', {
 
 // the colon is telling Iron Router that this part of the URL is a dynamic parameter.
 Router.route('/list/:_id', {
+  name: 'listPage',
   template: 'listPage',
   //By using the “data” option, we’re able to pass data into a route from within the associated function
   data: function() {
