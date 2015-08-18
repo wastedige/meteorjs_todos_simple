57b64c7c54c0697e92a7e1cacdf9603b8008e3a6
Shaahin Shahbazi <shaahin@gmail.com>
2015-08-18 14:10:33 -0500

    Force login for unauthorized access

--- routes.js
+++ routes.js
@@ -19,6 +19,15 @@ Router.route('/list/:_id', {
   data: function() {
     //console.log(this.params.someParameter);
     var currentList = this.params._id;
-    return Lists.findOne({ _id: currentList });
-  }
+    return Lists.findOne({ _id: currentList }); // will be used to fetch list's name
+  },
+  // hook! redirects user to login page instead of rendering the page if user's not logged in
+  onBeforeAction: function(){
+        var currentUser = Meteor.userId();
+        if(currentUser){
+            this.next(); // basically means proceed
+        } else {
+            this.render("login");
+        }
+    }
 });
